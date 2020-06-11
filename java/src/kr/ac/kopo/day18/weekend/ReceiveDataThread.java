869057bf0f3e34d6f87d4155f6def135932fcf3e
kimhyeju_aa <hyeju.aa@gmail.com>
2020-06-11 17:50:31 +0900

    [weekend Hw]

---
+++ java/src/kr/ac/kopo/day18/weekend/ReceiveDataThread.java
@@ -0,0 +1,54 @@
+package kr.ac.kopo.day18.weekend;
+
+import java.net.*;
+
+import java.io.*;
+
+class ReceiveDataThread implements Runnable {
+
+	Socket client;
+
+	BufferedReader ois;
+
+	String receiveData;
+
+	public ReceiveDataThread(Socket s, BufferedReader ois) {
+
+		client = s;
+
+		this.ois = ois;
+
+	}
+
+	public void run() {
+
+		try {
+
+			while ((receiveData = ois.readLine()) != null)
+				System.out.println(receiveData);
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		}
+
+		finally {
+
+			try {
+
+				ois.close();
+
+				client.close();
+
+			} catch (IOException e2) {
+
+				e2.printStackTrace();
+
+			}
+
+		}
+
+	}
+
+}
